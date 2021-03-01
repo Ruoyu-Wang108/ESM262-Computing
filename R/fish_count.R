f07d0f8eadbadae0c07c7357d9afbb1793231dc4
Ruoyu-Wang108 <865524384@qq.com>
2021-03-01 11:25:54 -0800

    GDLR QS RW Assignment4 done

---
+++ R/fish_count.R
@@ -0,0 +1,22 @@
+# ESM 262 Assignment 4
+# Gabriel De La Rosa, Quin Smith, Ruoyu Wang
+# function to count fish
+
+# Variable inputs
+# fish, a vector of different fish
+# hist.plot, whether to include a histogram as part of the output
+
+fish_count <- function(fish, hist.plot = FALSE){
+  fish = as.factor(fish)
+  most_fish = names(which.max(summary(fish)))
+  rarest_fish = names(which.min(summary(fish)))
+  total_fish = sum(summary(fish))
+  
+  
+  if(hist.plot) {
+    return(list(most = most_fish, rarest = rarest_fish, sum = total_fish, fish_plot = plot(fish)))
+  } else {
+    return(list(most = most_fish, rarest = rarest_fish, sum = total_fish))
+    }
+  
+}
